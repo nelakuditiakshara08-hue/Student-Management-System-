40b983edb0ca22391b516f999285e8afd16893ec
Nelakuditi Akshara <nelakuditiakshara08@gmail.com>
2026-07-02 21:16:37 +0530

    Added new files

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Student-Management-System-;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Student Management System!!");
+    }
+    
+}
---
+++ Student.java
@@ -0,0 +1,8 @@
+package Student-Management-System-;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    String branch;
+}
